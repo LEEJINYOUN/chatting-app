263ff8c5ee0785814a119b3757ff37de099bbc03
이진영 <swlsdud1005@naver.com>
2023-01-23 11:11:55 +0900

    데이터베이스 연결, 채팅 입력 기능

--- src/fbase.js
+++ src/fbase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore, addDoc, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -12,3 +13,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const authService = getAuth(app);
+export const dbService = getFirestore();
+export const dbAddDoc = addDoc;
+export const dbCollection = collection;
+export const dbGetDocs = getDocs;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dbService, dbAddDoc, dbCollection, dbGetDocs } from "fbase";
 
 const Home = function Home() {
-  return <div>Home</div>;
+  const [chatting, setChatting] = useState("");
+  const [chatContents, setChatContents] = useState([]);
+  const getChatting = async () => {
+    const querySnapshot = await dbGetDocs(dbCollection(dbService, "chatting"));
+    querySnapshot.forEach((document) => {
+      const chatObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setChatContents((prev) => [chatObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getChatting();
+  }, []);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const docRef = await dbAddDoc(dbCollection(dbService, "chatting"), {
+        chatting,
+        createdAt: Date.now(),
+      });
+      console.log("Document written with ID: ", docRef);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+    setChatting("");
+  };
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setChatting(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={chatting}
+          onChange={onChange}
+          placeholder="오늘 기분"
+          maxLength={120}
+        />
+        <input type="submit" value="제출" />
+      </form>
+      <div>
+        {chatContents.map((item, id) => (
+          <div key={id}>
+            <h4>{item.chatting}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
